4adbd2f9a714b61567853f9797bbfd3c23532a6c
ukorvl <ukorotovskiy@gmail.com>
2025-12-10 14:13:36 +0400

    feat: allow chain rpc urls override

--- frontend/src/lib/chains/rpc.ts
+++ frontend/src/lib/chains/rpc.ts
@@ -1,6 +1,12 @@
 import { getAddress } from "viem";
 import { getAvailableChains } from ".";
 
+export const customRpcMap: Record<number, string[]> = {
+  1: [
+    "https://ethereum.publicnode.com",
+  ],
+};
+
 export function getContractExplorerUrl(chainId: number, contractAddress: string) {
   const normalized = getAddress(contractAddress);
 
